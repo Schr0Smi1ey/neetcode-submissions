96f4f4591b1f8ed721e998454535e56f6ec820d7
Sarafat Karim <128882721+Schr0Smi1ey@users.noreply.github.com>
2026-07-28 10:35:32 +0600

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.cpp
@@ -0,0 +1,28 @@
+// Time Complexity	O(n^2⋅klogk)
+// Space Complexity	O(nk)
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        int n = strs.size();
+        unordered_map<string, vector<string>> mp;
+
+        for(auto s : strs) {
+            string key = "";
+            int fre[26] = {0};
+            for(auto ch : s) {
+                fre[ch - 'a']++;
+            }
+            for(auto f : fre) {
+                key += "#" + to_string(f);
+            }
+            mp[key].push_back(s);
+        }
+    
+        for(auto it : mp) {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
